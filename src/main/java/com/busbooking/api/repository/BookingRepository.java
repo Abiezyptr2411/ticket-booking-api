00e825b8b748dfe34038f2e99598b944a76f22fc
Abiezyptr2411 <joniyespapa041@gmail.com>
2025-02-23 12:03:30 +0700

    deleted comment line

--- src/main/java/com/busbooking/api/repository/BookingRepository.java
+++ src/main/java/com/busbooking/api/repository/BookingRepository.java
@@ -1,15 +1,3 @@
-// package com.busbooking.api.repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// import com.busbooking.api.model.Booking;
-
-// @Repository
-// public interface BookingRepository extends JpaRepository<Booking, Long> {
-// }
-
-
 package com.busbooking.api.repository;
 
 import java.util.Optional;
